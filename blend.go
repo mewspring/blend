eaee13de2620468b9f6a1bf256be95cdfe77bd7b
mewmew <rnd0x00@gmail.com>
2013-03-31 17:07:11 +0200

    blend: Initial commit.

---
+++ blend.go
@@ -0,0 +1,115 @@
+// Package blend implements parsing of Blender files.
+package blend
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+// Blend represents the information contained within a blend file. It contains a
+// file header and a slice of file blocks.
+type Blend struct {
+	Hdr    *Header
+	Blocks []*Block
+}
+
+// Parse parsers the provided blend file.
+func Parse(filePath string) (b *Blend, err error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	// Parse file header.
+	b = new(Blend)
+	b.Hdr, err = ParseHeader(f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse file blocks.
+	for {
+		block, err := b.ParseBlock(f)
+		if err != nil {
+			return nil, err
+		}
+		if block.Hdr.Code == CodeENDB {
+			break
+		}
+
+		b.Blocks = append(b.Blocks, block)
+	}
+
+	return b, nil
+}
+
+// A Header is present at the beginning of each blend file.
+type Header struct {
+	// Pointer size.
+	PtrSize int
+	// Byte order.
+	Order binary.ByteOrder
+	// Blender version.
+	Ver int
+}
+
+// ParseHeader parses and returns the header of a blend file.
+//
+// Example file header:
+//    "BLENDER_V100"
+//    //  0-6   magic ("BLENDER")
+//    //    7   pointer size ("_" or "-")
+//    //    8   endianness ("V" or "v")
+//    // 9-11   version ("100")
+func ParseHeader(r io.Reader) (hdr *Header, err error) {
+	buf := make([]byte, 12)
+	_, err = io.ReadFull(r, buf)
+	if err != nil {
+		return nil, err
+	}
+
+	// File identifier.
+	magic := string(buf[0:7])
+	if magic != "BLENDER" {
+		return nil, fmt.Errorf("blend.ParseHeader: invalid file identifier %q.", magic)
+	}
+
+	// Pointer size.
+	hdr = new(Header)
+	size := buf[7]
+	switch size {
+	case '_':
+		// _ = 4 byte pointer
+		hdr.PtrSize = 4
+	case '-':
+		// - = 8 byte pointer
+		hdr.PtrSize = 8
+	default:
+		return nil, fmt.Errorf("blend.ParseHeader: invalid pointer size character '%c'.", size)
+	}
+
+	// Byte order.
+	order := buf[8]
+	switch order {
+	case 'v':
+		// v = little endian
+		hdr.Order = binary.LittleEndian
+	case 'V':
+		// V = big endian
+		hdr.Order = binary.BigEndian
+	default:
+		return nil, fmt.Errorf("blend.ParseHeader: invalid byte order character '%c'.", order)
+	}
+
+	// Version.
+	hdr.Ver, err = strconv.Atoi(string(buf[9:12]))
+	if err != nil {
+		return nil, fmt.Errorf("blend.ParseHeader: invalid version; %s", err)
+	}
+
+	return hdr, nil
+}
---
+++ block.go
@@ -0,0 +1,201 @@
+package blend
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+)
+
+// A Block contains a header and a type dependent body.
+type Block struct {
+	Hdr *BlockHeader
+	// Body contains an *io.SectionReader which is later replaced with a concrete
+	// block type after it has been parsed.
+	Body interface{}
+}
+
+func (b *Blend) ParseBlock(f *os.File) (block *Block, err error) {
+	// Parse block header.
+	block = new(Block)
+	block.Hdr, err = b.ParseBlockHeader(f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Store section reader for block body.
+	off, err := f.Seek(block.Hdr.Size, os.SEEK_CUR)
+	if err != nil {
+		return nil, err
+	}
+	block.Body = io.NewSectionReader(f, off-block.Hdr.Size, block.Hdr.Size)
+
+	return block, nil
+}
+
+// BlockHeader contains information about the block's type and size.
+type BlockHeader struct {
+	// Code provides a rough type description of the block.
+	Code BlockCode
+	// Total length of the data after the block header.
+	Size int64
+	// Memory address of the structure when it was written to disk.
+	OldAddr uint64
+	// Index in the Structure DNA.
+	SDNAIndex int
+	// Number of structures located in this block.
+	Count int
+}
+
+// ParseBlockHeader parses and returns a file block header.
+//
+// Example file block header:
+//    44 41 54 41  E0 00 00 00  88 5E 9D 04  00 00 00 00    DATA.....^......
+//    F8 00 00 00  0E 00 00 00                              ........
+
+//    //   0-3   block code   ("DATA")
+//    //   4-7   size         (0x000000E0 = 224)
+//    //  8-15   old addr     (0x00000000049D5E88) // size depends on PtrSize.
+//    // 16-19   sdna index   (0x000000F8 = 248)
+//    // 20-23   count        (0x0000000E = 14)
+func (b *Blend) ParseBlockHeader(r io.Reader) (hdr *BlockHeader, err error) {
+	// Block code.
+	buf := make([]byte, 4)
+	_, err = io.ReadFull(r, buf)
+	if err != nil {
+		return nil, err
+	}
+	hdr = new(BlockHeader)
+	code := string(buf)
+	switch code {
+	case "BR\x00\x00":
+		hdr.Code = CodeBR
+	case "CA\x00\x00":
+		hdr.Code = CodeCA
+	case "DATA":
+		hdr.Code = CodeDATA
+	case "DNA1":
+		hdr.Code = CodeDNA1
+	case "ENDB":
+		hdr.Code = CodeENDB
+	case "GLOB":
+		hdr.Code = CodeGLOB
+	case "LA\x00\x00":
+		hdr.Code = CodeLA
+	case "MA\x00\x00":
+		hdr.Code = CodeMA
+	case "ME\x00\x00":
+		hdr.Code = CodeME
+	case "OB\x00\x00":
+		hdr.Code = CodeOB
+	case "REND":
+		hdr.Code = CodeREND
+	case "SC\x00\x00":
+		hdr.Code = CodeSC
+	case "SN\x00\x00":
+		hdr.Code = CodeSN
+	case "TE\x00\x00":
+		hdr.Code = CodeTE
+	case "TEST":
+		hdr.Code = CodeTEST
+	case "WM\x00\x00":
+		hdr.Code = CodeWM
+	case "WO\x00\x00":
+		hdr.Code = CodeWO
+	default:
+		return nil, fmt.Errorf("Header.ParseBlockHeader: block code %q not yet implemented.", code)
+	}
+
+	// Block size.
+	var x int32
+	err = binary.Read(r, b.Hdr.Order, &x)
+	if err != nil {
+		return nil, err
+	}
+	hdr.Size = int64(x)
+
+	// Old memory address.
+	switch b.Hdr.PtrSize {
+	case 4:
+		var x uint32
+		err = binary.Read(r, b.Hdr.Order, &x)
+		if err != nil {
+			return nil, err
+		}
+		hdr.OldAddr = uint64(x)
+	case 8:
+		var x uint64
+		err = binary.Read(r, b.Hdr.Order, &x)
+		if err != nil {
+			return nil, err
+		}
+		hdr.OldAddr = x
+	}
+
+	// SDNA index.
+	err = binary.Read(r, b.Hdr.Order, &x)
+	if err != nil {
+		return nil, err
+	}
+	hdr.SDNAIndex = int(x)
+
+	// Structure count.
+	err = binary.Read(r, b.Hdr.Order, &x)
+	if err != nil {
+		return nil, err
+	}
+	hdr.Count = int(x)
+
+	return hdr, nil
+}
+
+// BlockCode represents a rough type description of a block.
+type BlockCode int
+
+func (typ BlockCode) String() string {
+	var m = map[BlockCode]string{
+		CodeBR:   "BR",
+		CodeCA:   "CA",
+		CodeDATA: "DATA",
+		CodeDNA1: "DNA1",
+		CodeENDB: "ENDB",
+		CodeGLOB: "GLOB",
+		CodeLA:   "LA",
+		CodeMA:   "MA",
+		CodeME:   "ME",
+		CodeOB:   "OB",
+		CodeREND: "REND",
+		CodeSC:   "SC",
+		CodeSN:   "SN",
+		CodeTE:   "TE",
+		CodeTEST: "TEST",
+		CodeWM:   "WM",
+		CodeWO:   "WO",
+	}
+	s, ok := m[typ]
+	if !ok {
+		return "<unknown block code>"
+	}
+	return s
+}
+
+// Block codes.
+const (
+	CodeBR BlockCode = iota
+	CodeCA
+	CodeDATA
+	CodeDNA1
+	CodeENDB
+	CodeGLOB
+	CodeLA
+	CodeMA
+	CodeME
+	CodeOB
+	CodeREND
+	CodeSC
+	CodeSN
+	CodeTE
+	CodeTEST
+	CodeWM
+	CodeWO
+)
