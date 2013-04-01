edd4070e5b44dc2f4bb874e82b6c61d6826ca481
mewmew <rnd0x00@gmail.com>
2013-04-02 00:20:52 +0200

    block: Add more details about how package block can be generated.

--- block/block.go
+++ block/block.go
@@ -1,4 +1,19 @@
 // Package block implements parsing of blend file blocks.
+//
+// One unique feature of blend files is that they contain a full definition of
+// every structure used in it's file blocks. The structure definitions are
+// stored in the DNA block.
+//
+// All block structure definitions ("struct.go") and the block parsing logic
+// ("parse.go") have been generating by parsing the DNA block of
+// "testdata/block.blend".
+//
+// The tool which was used to generate these two files is located at:
+//    cmd/blendef
+//
+// More complex blend files may contain structures which are not yet defined in
+// this package. If so, use blendef to regenerate "struct.go" and "parse.go" for
+// the given blend file.
 package block
 
 import (
