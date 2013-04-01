d68adc93e29ba47720f62d04901a54c6cffc3605
mewmew <rnd0x00@gmail.com>
2013-04-01 23:58:12 +0200

    blend: Add example test case.

---
+++ blend_test.go
@@ -0,0 +1,34 @@
+package blend_test
+
+import (
+	"fmt"
+	"log"
+
+	blend "."
+)
+
+func ExampleBlend() {
+	// Parse blend file header and block headers.
+	b, err := blend.Parse("testdata/block.blend")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer b.Close()
+
+	// Parse DNA block.
+	dna, err := b.GetDNA()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Parse and output the body of block 11.
+	blk := b.Blocks[11]
+	err = blk.ParseBody(b.Hdr.Order, dna)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%#v\n", blk.Body)
+
+	// Output:
+	// &block.ScrVert{Next:0x0, Prev:0x49ced28, Newv:0x0, Vec:block.Vec2s{X:1920, Y:1053}, Flag:1, Editflag:1}
+}
